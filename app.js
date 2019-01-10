7ce5e9fe28aab449774f7579efaca815f0c5b7e4
l3oss123456 <oncelemon02@gmail.com>
2019-01-11 00:45:29 +0700

    Homework

---
+++ app.js
@@ -0,0 +1,41 @@
+var name = new Vue({
+    el: '#name',
+    data: {
+      message: ''
+    }
+  });
+
+  var age = new Vue({
+      el: '#age',
+      data: {
+          ages: null
+      }
+  })
+
+  var food = new Vue({
+    el: '#food',
+    data: {
+        checkedfood : []
+    }
+    })
+
+    var sex = new Vue({
+        el: '#sex',
+        data: {
+            sex:''
+        }
+    })
+
+    var career = new Vue({
+        el: '#career',
+        data: {
+            career:''
+        }
+    })
+
+    var show = new Vue({
+        el: '#show',
+        data: {
+            showmessage: true
+        }
+    })
