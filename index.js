ccaa7d6f36fbe7111f04eb67b61cec96551d9645
poramin7894 <5906021612101@fitm.kmutnb.ac.th>
2019-01-11 15:28:24 +0700

    vue

---
+++ index.js
@@ -0,0 +1,14 @@
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+     name:"",
+     ages:0,
+     foods:[],
+     gender:'',
+     job:"",
+     show:false
+
+    },
+    methods:{
+    }
+  })
